5f60aa0a355d4a438391830680ac98c2e27cb2a2
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-08-01 15:26:10 -0400

    Persistencia: UserDefaults

--- FacebookClone/Controllers/ViewController.swift
+++ FacebookClone/Controllers/ViewController.swift
@@ -11,10 +11,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailSwitch: UISwitch!
+    
+    private let emailKey = "email-key"
+    private let storage = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //De esta manera buscamos un string en los UserDefaults y lo asignamos.
+        if let storedEmail = storage.string(forKey: emailKey) {
+            emailTextField.text = storedEmail
+            emailSwitch.isOn = true
+        } else {
+            emailSwitch.isOn = false
+        }
     }
 
     @IBAction func loginButtonAction() {
@@ -23,12 +35,20 @@ class ViewController: UIViewController {
         let password = passwordTextField.text
         
         if email == "jackson@cuevas.com", password == "123" {
-            print("Bienvenido!")
+            if emailSwitch.isOn {
+                //De esta menera guardamos valores con llave y valor en los UserDefaults.
+                
+                storage.set(email, forKey: emailKey)
+            } else {
+                storage.removeObject(forKey: emailKey)
+            }
+            
             performSegue(withIdentifier: "home_segue", sender: nil)
         } else {
             print("Credenciales Inválidas.")
         }
     }
+    
     
 }
 
