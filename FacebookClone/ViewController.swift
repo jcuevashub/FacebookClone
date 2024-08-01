f65db40d8def9b5c136e16235ab4cb899e5dda87
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-31 21:44:55 -0400

    Initial Commit

---
+++ FacebookClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookClone
+//
+//  Created by Jackson Cuevas on 31/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
