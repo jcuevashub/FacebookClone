532d2dee5e55e394e333603ac080ad248865a509
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-08-01 07:01:57 -0400

    Utilizando controles en código

--- FacebookClone/ViewController.swift
+++ FacebookClone/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonAction() {
+        //1. Obterner los valores de los textfield.
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "jackson@cuevas.com", password == "123" {
+            print("Bienvenido!")
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        } else {
+            print("Credenciales Inválidas.")
+        }
+    }
+    
 }
 
