a2aec63948ab5e23cbdcb9cf6dc000be2d73494d
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-08-01 20:35:38 -0400

    CocoaPods

--- FacebookClone/Controllers/ViewController.swift
+++ FacebookClone/Controllers/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PopupDialog
 
 class ViewController: UIViewController {
 
@@ -45,10 +46,42 @@ class ViewController: UIViewController {
             
             performSegue(withIdentifier: "home_segue", sender: nil)
         } else {
-            print("Credenciales Inválidas.")
+            showErrorDialog()
         }
     }
     
+    
+    private func showErrorDialog() {
+        // Prepare the popup assets
+        let title = "Error"
+        let message = "Credenciales Inválidas."
+        let image = UIImage(named: "pexels-photo-103290")
+
+        // Create the dialog
+        let popup = PopupDialog(title: title, message: message, image: image)
+
+        // Create buttons
+        let buttonOne = CancelButton(title: "CANCEL") {
+            print("You canceled the car dialog.")
+        }
+
+        // This button will not the dismiss the dialog
+        let buttonTwo = DefaultButton(title: "ADMIRE CAR", dismissOnTap: false) {
+            print("What a beauty!")
+        }
+
+        let buttonThree = DefaultButton(title: "BUY CAR", height: 60) {
+            print("Ah, maybe next time :)")
+        }
+
+        // Add buttons to dialog
+        // Alternatively, you can use popup.addButton(buttonOne)
+        // to add a single button
+        popup.addButtons([buttonOne, buttonTwo, buttonThree])
+
+        // Present dialog
+        self.present(popup, animated: true, completion: nil)
+    }
     
 }
 
